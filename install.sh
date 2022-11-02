b5f2151bfb4aa4c27c2be72ba682b70f0998fb24
Dirk Kretschmann <kretschmanndi@gmail.com>
2022-11-02 19:16:56 +0100

    echo in install script

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ NVIM_CONFIG_DIR="$HOME/.config/nvim"
 mkdir -p "$NVIM_CONFIG_DIR"
 
 curl -sSf 'https://raw.githubusercontent.com/golmman/vim_config/main/init.vim' > "${NVIM_CONFIG_DIR}/init.vim"
+
+echo 'vim config updated'
